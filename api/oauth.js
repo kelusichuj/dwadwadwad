af293ef5709b4eb458219595c6747c6c014e7bbd
kelusichuj <janvivamra7227@gmail.com>
2025-09-30 18:40:18 +0200

    Update oauth.js

--- api/oauth.js
+++ api/oauth.js
@@ -6,7 +6,7 @@ const supabase = createClient(
   ''
 );
 
-const guildId = ''; // <- ID twojego serwera
+const guildId = '1238834836475740180'; // <- ID twojego serwera
 const botToken = process.env.DISCORD_BOT_TOKEN; // <- Dodaj do .env lub wpisz ręcznie (jeśli testujesz)
 
 export default async function handler(req, res) {
@@ -74,3 +74,4 @@ export default async function handler(req, res) {
     return res.status(500).send('Błąd logowania');
   }
 }
+
